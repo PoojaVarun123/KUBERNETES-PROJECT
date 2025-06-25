3ad149c24c7d0ea88a4e500718893cb2debe38bf
PoojaVarun123 <poojaas0711@gmail.com>
2025-06-25 22:57:51 +0530

    Update alb.tf

--- Terraform/K8S_Topics/alb/alb.tf
+++ Terraform/K8S_Topics/alb/alb.tf
@@ -1,1 +1,88 @@
+==========================================================================================
+                                 OIDC PROVIDER
+==========================================================================================
+data "aws_eks_cluster" "eks" {
+  name = "eks-prod"
+}
 
+data "tls_certificate" "oidc_thumbprint" {
+  url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
+  url             = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+-------------------------------------------------------------------------------------------
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = "https://oidc.eks.us-east-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B716D3041E"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "9e99a48a9960b14926bb7f3b02e22da0afd10df6"
+  ]
+}
+============================================================================================
+                                           IRSA ROLE
+=============================================================================================
+--Create IAM Role for ALB Controller (IRSA)
+
+resource "aws_iam_role" "alb_controller" {
+  name = "AmazonEKSLoadBalancerControllerRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(data.aws_eks_cluster.eks.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+✅ Attach Policy
+
+resource "aws_iam_policy_attachment" "alb_policy" {
+  name       = "alb-controller-attach"
+  roles      = [aws_iam_role.alb_controller.name]
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+}
+--------------------------------------------------------------------------------------------
+resource "aws_iam_role" "alb_controller" {
+  name = "AmazonEKSLoadBalancerControllerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::123456789012:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B716D3041E"
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "oidc.eks.us-east-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B716D3041E:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+======================================================================================
+✅ 3. IAM Policy Attachment
+======================================================================================
+resource "aws_iam_role_policy_attachment" "alb_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+  role       = aws_iam_role.alb_controller.name
+}
+=======================================================================================
