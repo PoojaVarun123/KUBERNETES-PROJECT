27cfc08fa51eafe35c1e81e607c50400e753220b
PoojaVarun123 <poojaas0711@gmail.com>
2025-06-25 23:01:03 +0530

    Update oidc.tf

--- Terraform/K8S_Topics/alb/alb-full/oidc.tf
+++ Terraform/K8S_Topics/alb/alb-full/oidc.tf
@@ -1,1 +1,17 @@
+======================================================================================================
+                                     OIDC PROVIDER
+======================================================================================================
+data "aws_eks_cluster" "eks" {
+  name = "eks-prod"
+}
 
+data "tls_certificate" "oidc_thumbprint" {
+  url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
+  url             = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+=======================================================================================================
