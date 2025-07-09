133ac171b4b7faeaa42253a87d580bd99146f680
PoojaVarun123 <poojaas0711@gmail.com>
2025-07-09 13:17:35 +0530

    Create github.tf

---
+++ github/github.tf
@@ -0,0 +1,35 @@
+1) Github > decentralized > open source source code managemnet tool > verison control system > enables collaboration,parrallel development,manage source code history
+2) Git
+Git is a version control system to manage source code history. 
+GitHub is a hosting service for Git repositories online
+
+3)git branching strategy > gitfflow model
+  main > permanent
+  develop > permanent -- staging/QA
+        feature-1
+        feature-2
+        release -- PRODUCT
+        bugfix
+        hotfix 
+4) git stash > pop,list
+5) git pull
+6) git push
+7) git fetch
+8) git merge
+9)  git rebase
+10) git clone
+11) git remote
+12) git config 
+13) git add 
+14) git commit
+15) git squash
+16) git cherry pick
+17) git checkout
+18) git blame 
+19) git diff commit_1 coomit_2
+20) git restore > unstage 
+21) git revert > undo commit
+22) git reset hard soft 
+21) git log /status
+22) git status
+23) conflict 
