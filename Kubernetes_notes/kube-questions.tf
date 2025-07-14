79b88d7a325e080aefb342eac446a38c07b32efa
PoojaVarun123 <poojaas0711@gmail.com>
2025-07-14 23:04:27 +0530

    Create kube-questions.tf

---
+++ Kubernetes_notes/kube-questions.tf
@@ -0,0 +1,18 @@
+what is kubernetes?
+kubernetes architecture?
+what is pod?
+   - smallest deployment unit and wrapper of pod
+init and sidecar?
+what is deployment?
+what is service?
+what is label/selector?
+  -A label is a key-value pair that you attach to Kubernetes objects (like Pods, Nodes, Services, etc.) to organize and identify them.
+  -A selector is used to find and match Kubernetes objects based on their labels.
+For example, a Service uses a selector to send traffic only to the pods with matching labels.
+  External name :A Kubernetes service that acts as a DNS alias, mapping a service name inside the cluster to an external DNS name outside the cluster.
+Why service is important?
+how to troubleshoot pod?
+deployment strategy? rolling update + blue and canary 
+maxunavialabe
+maxsurge
+
